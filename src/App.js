550146b02e2134ec16c026092e3f1446d61c3a59
Rafael <rafa.comp@gmail.com>
2019-03-02 16:27:15 -0300

    criacao da pagina

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React, { Fragment } from 'react';
+import Main from './pages/Main';
 import GlobalStyle from './styles/global';
 
 const App = () => (
   <Fragment>
     <GlobalStyle />
-    <div className="App" />
+    <Main />
   </Fragment>
 );
 
