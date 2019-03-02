ce7218664f6c563743683fd668d23e0f0e238f01
Rafael <rafa.comp@gmail.com>
2019-03-02 16:57:53 -0300

    estilizacao da lista de repositorios

--- src/components/CompareList/index.js
+++ src/components/CompareList/index.js
@@ -1,31 +1,35 @@
 import React from 'react';
 
+import { Container, Repository } from './styles';
+
 const CompareList = () => (
-  <div>
-    <header>
-      <img src="https://avatars3.githubusercontent.com/u/69631?v=4" alt="facebook" />
-      <strong>react</strong>
-      <small>facebook</small>
-    </header>
-    <ul>
-      <li>
-        96,019
-        <small>stars</small>
-      </li>
-      <li>
-        96,019
-        <small>forks</small>
-      </li>
-      <li>
-        96,019
-        <small>issues</small>
-      </li>
-      <li>
-        3 days ago
-        <small>last commit</small>
-      </li>
-    </ul>
-  </div>
+  <Container>
+    <Repository>
+      <header>
+        <img src="https://avatars3.githubusercontent.com/u/69631?v=4" alt="facebook" />
+        <strong>react</strong>
+        <small>facebook</small>
+      </header>
+      <ul>
+        <li>
+          96,019
+          <small>stars</small>
+        </li>
+        <li>
+          96,019
+          <small>forks</small>
+        </li>
+        <li>
+          96,019
+          <small>issues</small>
+        </li>
+        <li>
+          3 days ago
+          <small>last commit</small>
+        </li>
+      </ul>
+    </Repository>
+  </Container>
 );
 
 export default CompareList;
